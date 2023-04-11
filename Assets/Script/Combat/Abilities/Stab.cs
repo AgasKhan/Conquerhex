4d853c594ec90dde34fbb52f253148afc6c6433e
AgasKhan <lucas_galardo_8@yahoo.com.ar>
2023-04-11 17:09:05 -0300

    combat update v3
    -se aniadieron comentarios

--- Assets/Script/Combat/Abilities/Stab.cs
+++ Assets/Script/Combat/Abilities/Stab.cs
@@ -5,37 +5,25 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Abilities/Stab")]
 public class Stab : AbilityBase
 {
-<<<<<<< Updated upstream
-    //¿Cuando?
-    public override void ControllerDown(Vector2 dir, float button, WeaponBase weapon, bool cooldownEnd)
-=======
+    //cuandos
+
     public override void ControllerDown(Vector2 dir, float button, WeaponBase weapon, Timer cooldownEnd)
->>>>>>> Stashed changes
     {
 
     }
 
-<<<<<<< Updated upstream
-    //¿Cuando?
-    public override void ControllerPressed(Vector2 dir, float button, WeaponBase weapon, bool cooldownEnd)
-=======
+
     public override void ControllerPressed(Vector2 dir, float button, WeaponBase weapon, Timer cooldownEnd)
->>>>>>> Stashed changes
     {
 
     }
 
-<<<<<<< Updated upstream
-    //¿Cuando?
-    public override void ControllerUp(Vector2 dir, float button, WeaponBase weapon, bool cooldownEnd)
-=======
     public override void ControllerUp(Vector2 dir, float button, WeaponBase weapon, Timer cooldownEnd)
->>>>>>> Stashed changes
     {
 
     }
 
-    //¿Como?
+    //como
     protected override void InternalAttack(Vector2 direction, WeaponBase weapon)
     {
         Debug.Log(direction);
