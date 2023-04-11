8444404b637b55238a5ff088aadc1217ff5ad1de
Deldryhe <ivan.zbrun@davinci.edu.ar>
2023-04-11 16:52:26 -0300

    Update Stab.cs

--- Assets/Script/Combat/Abilities/Stab.cs
+++ Assets/Script/Combat/Abilities/Stab.cs
@@ -5,21 +5,25 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Abilities/Stab")]
 public class Stab : AbilityBase
 {
+    //¿Cuando?
     public override void ControllerDown(Vector2 dir, float button, WeaponBase weapon, bool cooldownEnd)
     {
         throw new System.NotImplementedException();
     }
 
+    //¿Cuando?
     public override void ControllerPressed(Vector2 dir, float button, WeaponBase weapon, bool cooldownEnd)
     {
         throw new System.NotImplementedException();
     }
 
+    //¿Cuando?
     public override void ControllerUp(Vector2 dir, float button, WeaponBase weapon, bool cooldownEnd)
     {
         throw new System.NotImplementedException();
     }
 
+    //¿Como?
     protected override void InternalAttack(Vector2 direction, WeaponBase weapon)
     {
         Debug.Log(direction);
