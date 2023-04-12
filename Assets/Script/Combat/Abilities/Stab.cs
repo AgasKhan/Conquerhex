71edd7e6cddb99b280f272a4a03468d6b76b873c
Deldryhe <ivan.zbrun@davinci.edu.ar>
2023-04-12 15:43:54 -0300

    Stab 0.1
    - se comenzó a plantear la primera habilidad

--- Assets/Script/Combat/Abilities/Stab.cs
+++ Assets/Script/Combat/Abilities/Stab.cs
@@ -5,27 +5,45 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Abilities/Stab")]
 public class Stab : AbilityBase
 {
-    //cuandos
-
+    //Cuandos
+    //Antes
     public override void ControllerDown(Vector2 dir, float button, Weapon weapon, Timer cooldownEnd)
     {
-
+        //Pocision en la mano del pj sin moverse ni extenderse
+        //dir.y = 0;
+        //dir.x = 0;
     }
 
-
+    //Durante
     public override void ControllerPressed(Vector2 dir, float button, Weapon weapon, Timer cooldownEnd)
     {
+        //Posicion extendida, moviendose en horizontal al apuñalar 
+        //dir.x = 1;
+        //dir.y = 0;
 
+        weapon.Durability();
     }
 
+    //Despues
     public override void ControllerUp(Vector2 dir, float button, Weapon weapon, Timer cooldownEnd)
     {
+        //Vuelve a posicion inicial
+        //dir.x = 0;
+        //dir.y = 0;
+
+        //comienza a bajar el cooldown
 
     }
 
-    //como
+    //Como
     protected override void InternalAttack(Vector2 direction, Damage[] damages)
     {
         Debug.Log(direction);
+
+        direction.x = 1;
+        direction.y = 0;
+
+
+
     }
 }
