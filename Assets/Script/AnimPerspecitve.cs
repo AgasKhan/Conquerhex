9c790030dbee7a72c479d0ace2a038c3c8405739
AgasKhan <lucas_galardo_8@yahoo.com.ar>
2023-04-14 17:30:52 -0300

    fix de shadows
    -se corrigieron algunos bugs debidos al shader creado para las sombras
    
    -se corrigieron algunos prefabs que tenian mal su escala en Z
    
    -se agrego la actualizacion automatica de las sombras en base al sprite

--- Assets/Script/AnimPerspecitve.cs
+++ Assets/Script/AnimPerspecitve.cs
@@ -17,8 +17,43 @@ public class AnimPerspecitve : MonoBehaviour
     public string sortingLayer;
 
     [SerializeField]
-    SpriteRenderer sprite;
+    SpriteRenderer originalSprite;
 
+    SpriteRenderer shadowSprite;
+    
+
+    private void Awake()
+    {
+        originalSprite = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    void CreateShadow()
+    {
+        shadowSprite = Instantiate(originalSprite, transform);
+
+        shadowSprite.material = material;
+
+        originalSprite.RegisterSpriteChangeCallback(UpdateShadowSprite);
+    }
+
+    void UpdateShadowSprite(SpriteRenderer sprite)
+    {
+        shadowSprite.sprite = sprite.sprite;
+        shadowSprite.flipX = sprite.flipX;
+    }
+
+    void UpdateShadow()
+    {
+        shadowSprite.material.SetVector("_Vector2", dirVector);
+
+        shadowSprite.material.SetColor("_Color", colorShadow);
+
+        shadowSprite.sortingLayerName = sortingLayer;
+
+        shadowSprite.gameObject.transform.rotation = Quaternion.identity;
+
+        
+    }
 
     private void OnEnable()
     {
@@ -27,21 +62,9 @@ public class AnimPerspecitve : MonoBehaviour
         if (!shadow)
             return;
 
+        if(shadowSprite==null)
+            CreateShadow();
 
-        if(sprite==null)
-        {
-            sprite = GetComponentInChildren<SpriteRenderer>();
-
-            sprite = Instantiate(sprite, transform);
-
-            sprite.material = material;
-        }
-                
-
-        sprite.material.SetVector("_Vector2", dirVector);
-
-        sprite.material.SetColor("_Color", colorShadow);
-
-        sprite.sortingLayerName = sortingLayer;
+        UpdateShadow();
     }
 }
