64046ff05cc222a01a82e60e42f126f9936cf196
Deldryhe <ivan.zbrun@davinci.edu.ar>
2023-04-20 13:33:34 -0300

    Kick v1 & Cut v1
    -Se crearon las habilidades de Kick y Cut

--- Assets/Script/Combat/Abilities/Cut.cs
+++ Assets/Script/Combat/Abilities/Cut.cs
@@ -2,14 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-[CreateAssetMenu(menuName = "Abilities/Kick")]
-public class Kick : AbilityBase
+[CreateAssetMenu(menuName = "Abilities/Cut")]
+public class Cut : AbilityBase
 {
-    private bool isKicking = false;
+    private bool isCuting = false; 
 
-    private float kickRange;
-
+    private float cutRange;
     /*
     Entity caster: ENTIDAD QUE USA LA HABILIDAD
     Vector2 dir: HACIA DONDE APUNTA LA HABILIDAD
@@ -20,24 +18,24 @@ public class Kick : AbilityBase
 
     public override void ControllerDown(Entity caster, Vector2 dir, float button, Weapon weapon, Timer cooldownEnd)
     {
-        cooldownEnd.Set(5f, true);
+        cooldownEnd.Set(3.5f, true);
 
-        isKicking = true;
+        isCuting = true; 
 
     }
 
     public override void ControllerPressed(Entity caster, Vector2 dir, float button, Weapon weapon, Timer cooldownEnd)
     {
-        dir = dir.normalized;
+        dir = dir.normalized; 
 
         InternalAttack(caster, dir, damages);
 
-        weapon.Durability(3);
+        weapon.Durability(5);
     }
 
     public override void ControllerUp(Entity caster, Vector2 dir, float button, Weapon weapon, Timer cooldownEnd)
     {
-        isKicking = true;
+        isCuting = false;
 
         cooldownEnd.Start();
 
@@ -46,9 +44,9 @@ public class Kick : AbilityBase
 
     protected override void InternalAttack(Entity caster, Vector2 direction, Damage[] damages)
     {
-        if (isKicking && direction == direction.normalized)
+        if (isCuting && direction == direction.normalized)
         {
-            RaycastHit[] hits = Physics.SphereCastAll(caster.transform.position, kickRange, direction, 0f);
+            RaycastHit[] hits = Physics.SphereCastAll(caster.transform.position, cutRange, direction, 0f);
 
             foreach (RaycastHit hit in hits)
             {
