19e8c72518eb1321aa013a2853b52f4dc13b376e
AgasKhan <lucas_galardo_8@yahoo.com.ar>
2023-04-22 04:27:30 -0300

    Reordenamiento
    -Se ordenaron los scripteables objects
    
    -Se agrego al MoveTr que cambie el sort order del sprite en base a su Y
    
    -Se crearon carpetas para ordenar las categorias de scripts

--- Assets/Script/Movement/MoveTr.cs
+++ Assets/Script/Movement/MoveTr.cs
@@ -4,9 +4,23 @@ using UnityEngine;
 
 public class MoveTr : MoveAbstract
 {
+    SpriteRenderer rend;
+
     protected override void Config()
     {
+        MyAwakes += MyAwake;
         MyUpdates += MyUpdate;
+
+        if (rend != null)
+            MyUpdates += MyUpdateRender;
+    }
+
+    void MyAwake()
+    {
+        rend = GetComponentInChildren<SpriteRenderer>();
+
+        if (rend != null)
+            MyUpdates += MyUpdateRender;
     }
 
     public void MyUpdate()
@@ -14,6 +28,11 @@ public class MoveTr : MoveAbstract
         transform.position += (direction * _velocity.current * Time.deltaTime).Vec2to3(0);
 
         _velocity.Substract(_desaceleration.current * Time.deltaTime);
+    }
+
+    void MyUpdateRender()
+    {
+        rend.sortingOrder = Mathf.RoundToInt(transform.position.y * -100);
     }
 
 }
