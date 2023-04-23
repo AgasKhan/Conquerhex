c1c7babb19d89c70ef09193a6d5f5a1cb4010516
Karevelli <revellikarina@hotmail.com>
2023-04-23 05:52:29 -0300

    FoV
    - Se implementa un FoV para usar con las IA

--- Assets/Script/IA/Enemy/EnemyMove.cs
+++ Assets/Script/IA/Enemy/EnemyMove.cs
@@ -5,7 +5,7 @@ using System;
 
 public class EnemyMove : Seek, IControllerDir
 {
-    [SerializeField] float _viewRadius;
+    //[SerializeField] float _viewRadius;
     [SerializeField] Transform[] _totalWaypoints;
 
     int _currentWaypoint;
@@ -26,24 +26,24 @@ public class EnemyMove : Seek, IControllerDir
 
     public void ControllerPressed(Vector2 dir, float tim)
     {
-        Transform nextWaypoint = _totalWaypoints[_currentWaypoint];
-        Vector2 dirToWaypoint = DirectionSeek(move.Director(nextWaypoint.position));
+        //Transform nextWaypoint = _totalWaypoints[_currentWaypoint];
+        //Vector2 dirToWaypoint = DirectionSeek(move.Director(nextWaypoint.position));
 
-        //Si no está dentro del rango de visión, patrulla
-        if (_viewRadius * _viewRadius >= dirToWaypoint.sqrMagnitude)
-        {
+        ////Si no está dentro del rango de visión, patrulla
+        //if (_viewRadius * _viewRadius >= dirToWaypoint.sqrMagnitude)
+        //{
 
-            _OnCurrentPath();
-            //ControllerPressed(dirToWaypoint, 0);
+        //    _OnCurrentPath();
+        //    //ControllerPressed(dirToWaypoint, 0);
 
-        }
+        //}
 
-        //Sino, persigue
-        else
-        {
-            DirectionPursuit(move.Director(_target.position));
-            Debug.Log("Pursuit");
-        }
+        ////Sino, persigue
+        //else
+        //{
+        //    DirectionPursuit(move.Director(_target.position));
+        //    Debug.Log("Pursuit");
+        //}
         
 
     }
@@ -78,11 +78,11 @@ public class EnemyMove : Seek, IControllerDir
     }
 
 
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.white;
-        Gizmos.DrawWireSphere(transform.position, Mathf.Sqrt(_viewRadius));
-    }
+    //private void OnDrawGizmos()
+    //{
+    //    Gizmos.color = Color.white;
+    //    Gizmos.DrawWireSphere(transform.position, Mathf.Sqrt(_viewRadius));
+    //}
 
     private void Update()
     {
---
+++ Assets/Script/IA/FoV/FoVEditor.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor (typeof (FieldOfView))]
+public class FoVEditor : Editor
+{
+    private void OnSceneGUI()
+    {
+        FieldOfView fow = (FieldOfView)target;
+        Handles.color = Color.white;
+        //Circulo
+        Handles.DrawWireArc(fow.transform.position, Vector3.forward, Vector3.right, 360, fow.viewRadius);
+        //angulos
+        Vector2 viewAngleA = fow.DirFromAngle(-fow.viewAngle / 2, false);
+        Vector2 viewAngleB = fow.DirFromAngle(fow.viewAngle / 2, false);
+
+        //longitud de la línea
+        Handles.DrawLine(fow.transform.position, (fow.transform.position).Vect3To2() + viewAngleA * fow.viewRadius);
+        Handles.DrawLine(fow.transform.position, (fow.transform.position).Vect3To2() + viewAngleB * fow.viewRadius);
+
+        Handles.color = Color.red;
+        foreach (Transform visibleTarget in fow.visibleTargets)
+        {
+            Handles.DrawLine(fow.transform.position, visibleTarget.position);
+        }
+
+    }
+
+}
