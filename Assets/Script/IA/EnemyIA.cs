d6a8a606e73517265d6f30e8cab72f648f19f7bc
Karevelli <revellikarina@hotmail.com>
2023-04-26 07:28:24 -0300

    IA
    -no funcional
    Está comentado para no tener errores y continuarlo luego

--- Assets/Script/IA/EnemyIA.cs
+++ Assets/Script/IA/EnemyIA.cs
@@ -5,91 +5,113 @@ using System;
 
 public class EnemyIA : IAFather
 {
-    MoveAbstract move;
+    //MoveAbstract move;
 
     Seek seek;
     Persuit pursuit;
 
-    GameObject target;
+    [SerializeField]
+    MoveAbstract me;
+    [SerializeField]
+    MoveAbstract target; 
+    
+    //GameObject target;
 
     [SerializeField] float _viewRadius;
     [SerializeField] float _detectionRadius;
-    [SerializeField] Transform[] _totalWaypoints;
 
-    int _currentWaypoint;
+    WeaponKataBase wb;
+    Character character;
+    //[SerializeField] Transform[] _totalWaypoints;
 
-    [SerializeField] Transform _target;
+    //int _currentWaypoint;
 
-    Action _OnCurrentPath;
+
+
+    //Transform _target;
+
+    //Action _OnCurrentPath;
 
     private void Start()
     {
-        
+
     }
 
-    void NormalPath()
-    {
-        _currentWaypoint++;
+    #region patrol
+    //void NormalPath()
+    //{
+    //    _currentWaypoint++;
 
-        if (_currentWaypoint >= _totalWaypoints.Length)
-        {
-            _currentWaypoint--;
+    //    if (_currentWaypoint >= _totalWaypoints.Length)
+    //    {
+    //        _currentWaypoint--;
 
-            _OnCurrentPath = BackwardPath;
-        }
-    }
+    //        _OnCurrentPath = BackwardPath;
+    //    }
+    //}
 
-    void BackwardPath()
-    {
-        _currentWaypoint--;
+    //void BackwardPath()
+    //{
+    //    _currentWaypoint--;
 
-        if (_currentWaypoint < 0)
-        {
-            _currentWaypoint++;
+    //    if (_currentWaypoint < 0)
+    //    {
+    //        _currentWaypoint++;
 
-            _OnCurrentPath = NormalPath;
-        }
-    }
+    //        _OnCurrentPath = NormalPath;
+    //    }
+    //}
 
 
 
     //Patrol
-    public override void OnEnterState(Character param)
-    {
-        Transform nextWaypoint = _totalWaypoints[_currentWaypoint];
-        Vector2 dirToWaypoint = seek.Calculate(move.Director(nextWaypoint.position));
 
-        if (_viewRadius * _viewRadius >= dirToWaypoint.sqrMagnitude)
-        {
-            _OnCurrentPath();
+    //Transform nextWaypoint = _totalWaypoints[_currentWaypoint];
+    //Vector2 dirToWaypoint = seek.Calculate(move.Director(nextWaypoint.position));
 
-        }
-    }
+    //if (_viewRadius * _viewRadius >= dirToWaypoint.sqrMagnitude)
+    //{
+    //    _OnCurrentPath();
+
+    //}
+    //Return
+    //BackwardPath();
+
 
     //Detection
-    public override void OnStayState(Character param)
+    #endregion
+    public override void OnEnterState(Character param)
     {
+        /*Vector2 dirToTarget = seek.Calculate(me.Director(target.transform.position));
 
-        Vector2 dirToTarget = seek.Calculate(move.Director(target.transform.position));
+        Debug.Log("dittotarget " + dirToTarget);
 
         if (dirToTarget.sqrMagnitude <= _viewRadius * _viewRadius)
         {
-            pursuit.Calculate(move.Director(target.transform.position));
+            var aux = pursuit.Calculate(me.Director(target.transform.position));
+            me.ControllerPressed(aux, 0);
 
             if (dirToTarget.sqrMagnitude <= _detectionRadius * _detectionRadius)
             {
-                //Attack
+                wb.IADetect(character, target.transform.position);
+
+                Debug.Log("attack-dirto " + dirToTarget);
             }
         }
-
+        */
     }
 
-    //Return
+    public override void OnStayState(Character param)
+    {
+        //character = param;
+
+        //var aux = seek.Calculate(target);
+        //me.ControllerPressed(aux, 1);
+    }
+
     public override void OnExitState(Character param)
     {
-        BackwardPath();
     }
-
 
     private void OnDrawGizmos()
     {
@@ -99,20 +121,7 @@ public class EnemyIA : IAFather
 
 
 
-    //public void Patrol()
-    //{
-
-    //}
-
-    //public void Detection()
-    //{
-    //}
-
-    //public void Return()
-    //{
-    //}
 }
-
 
 
 
