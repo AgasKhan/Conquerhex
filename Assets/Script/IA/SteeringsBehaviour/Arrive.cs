5350ae83d5e474615aee59eb1060b82851a08584
Karevelli <revellikarina@hotmail.com>
2023-04-28 00:17:38 -0300

    arrive
    arrive arreglado
    corderito para justificar la paga del sueldo

--- Assets/Script/IA/SteeringsBehaviour/Arrive.cs
+++ Assets/Script/IA/SteeringsBehaviour/Arrive.cs
@@ -7,17 +7,49 @@ public class Arrive : SteeringBehaviour
 
     public override Vector2 Calculate(MoveAbstract target)
     {
-        _desiredVelocity = Vector2.ClampMagnitude(target.transform.position, me.maxSpeed);
+        Vector2 desired = Direction(target);
+        var speed = me.maxSpeed;
 
-        if (_desiredVelocity.sqrMagnitude < (me.velocity * me.velocity / (me._desaceleration.current * me._desaceleration.current)))
-            _desiredVelocity = -me.vectorVelocity * (me._desaceleration.current - 1);
+        if (desired.sqrMagnitude <= (me.velocity * me.velocity / (me._desaceleration.current * me._desaceleration.current)))
+            speed = -me.maxSpeed * ((desired.magnitude - 1) / me._desaceleration.current);
 
-        _steering = _desiredVelocity - me.vectorVelocity;
+        desired *= speed;
 
-        var vecVelocity = me.vectorVelocity;
+        Vector2 st = desired - me.vectorVelocity;
+        st = Vector2.ClampMagnitude(st, me.maxSpeed);
 
-        vecVelocity += _steering * Time.deltaTime;
+        return st;
 
-        return vecVelocity;
+
+        //original
+        //_desiredVelocity = Vector2.ClampMagnitude(target.transform.position, me.maxSpeed);
+
+        //if (_desiredVelocity.sqrMagnitude < (me.velocity * me.velocity / (me._desaceleration.current * me._desaceleration.current)))
+        //    _desiredVelocity = -me.vectorVelocity * (me._desaceleration.current - 1);
+
+        //_steering = _desiredVelocity - me.vectorVelocity;
+
+        //var vecVelocity = me.vectorVelocity;
+
+        //vecVelocity += _steering * Time.deltaTime;
+
+        //return vecVelocity;
+
+        //Vector2 desired = Direction(target);
+
+
+        //ver arreglada version1
+        //Vector2 desired = Direction(target);
+        //if (desired.sqrMagnitude < (me.velocity * me.velocity / (me._desaceleration.current * me._desaceleration.current)))
+        //    desired = -me.vectorVelocity * (me._desaceleration.current - 1);
+
+        //_steering = desired - me.vectorVelocity;
+
+        //var vecVelocity = me.vectorVelocity;
+        //vecVelocity += _steering;
+
+        //vecVelocity = Vector2.ClampMagnitude(vecVelocity, me.maxSpeed);
+
+        //return vecVelocity;
     }
 }
