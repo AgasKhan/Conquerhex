ab6edecc87d0f56e2f5adbe4d475e6c1a116aa2a
Karevelli <revellikarina@hotmail.com>
2023-04-28 00:19:26 -0300

    ajuste

--- Assets/Script/IA/SteeringsBehaviour/Arrive.cs
+++ Assets/Script/IA/SteeringsBehaviour/Arrive.cs
@@ -20,36 +20,5 @@ public class Arrive : SteeringBehaviour
 
         return st;
 
-
-        //original
-        //_desiredVelocity = Vector2.ClampMagnitude(target.transform.position, me.maxSpeed);
-
-        //if (_desiredVelocity.sqrMagnitude < (me.velocity * me.velocity / (me._desaceleration.current * me._desaceleration.current)))
-        //    _desiredVelocity = -me.vectorVelocity * (me._desaceleration.current - 1);
-
-        //_steering = _desiredVelocity - me.vectorVelocity;
-
-        //var vecVelocity = me.vectorVelocity;
-
-        //vecVelocity += _steering * Time.deltaTime;
-
-        //return vecVelocity;
-
-        //Vector2 desired = Direction(target);
-
-
-        //ver arreglada version1
-        //Vector2 desired = Direction(target);
-        //if (desired.sqrMagnitude < (me.velocity * me.velocity / (me._desaceleration.current * me._desaceleration.current)))
-        //    desired = -me.vectorVelocity * (me._desaceleration.current - 1);
-
-        //_steering = desired - me.vectorVelocity;
-
-        //var vecVelocity = me.vectorVelocity;
-        //vecVelocity += _steering;
-
-        //vecVelocity = Vector2.ClampMagnitude(vecVelocity, me.maxSpeed);
-
-        //return vecVelocity;
     }
 }
