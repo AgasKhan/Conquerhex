2784aa8e000216e4103f7c8b4d99128f02787487
JhamilCastillo <jhamil.castillo@davinci.edu.ar>
2023-04-29 02:12:36 -0400

    Cazadoroso

--- Assets/Script/IA/IABoid.cs
+++ Assets/Script/IA/IABoid.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class IABoid : IAFather
 {
     [SerializeField]
-    Detect<Entity> detect;
+    protected Detect<Entity> detect;
 
     [SerializeField]
     public Pictionarys<string,SteeringWithTarger> steerings;
---
+++ Assets/Script/IA/IAHunter.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IAHunter : IABoid
+{
+
+    public override void OnEnterState(Character param)
+    {
+
+    }
+
+    public override void OnExitState(Character param)
+    {
+
+    }
+
+    public override void OnStayState(Character param)
+    {
+
+        //pendiente: necesito el area para que chequee el mas cercano + chequear que no interfiera con el area de detección del arrive
+        var corderos = detect.Area(param.transform.position, (target) => { return Team.hervivoro == target.team; });
+
+        //añado los corderos que estan en mi area de detección, si ya esta en la lista no se añade
+        steerings["corderitos"].targets = corderos;
+
+
+        foreach (var itemInPictionary in steerings)
+        {
+            for (int i = 0; i < itemInPictionary.value.Count; i++)
+            {
+                param.move.Acelerator(itemInPictionary.value.steering.Calculate(itemInPictionary.value[i]));
+            }
+        }
+    }
+
+}
