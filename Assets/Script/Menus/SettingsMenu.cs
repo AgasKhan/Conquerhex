53f183d309b0767b8ce22ed2bba5e3a321d9965b
JhamilCastillo <jhamil.castillo@davinci.edu.ar>
2023-05-10 03:25:45 -0400

    Update Menu Manager
    - Ahora el menu manager es quien cambia los volúmenes del mixer
    - Cada vez que se cambia el volumen se guarda su valor en el BD
    - Al empezar cada escena se verifica si hay un valor guardado de volumen en el BD y si lo hay, se modifica

--- Assets/Script/Menus/SettingsMenu.cs
+++ Assets/Script/Menus/SettingsMenu.cs
@@ -1,18 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
-public class SettingsMenu : MonoBehaviour
+public class SettingsMenu : SingletonMono<SettingsMenu>
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
