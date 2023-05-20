51cafba71d839a92040af520bde5ee029b2aa89d
AgasKhan <lucas_galardo_8@yahoo.com.ar>
2023-05-19 21:41:58 -0300

    Events Calls Update
    -se implementaron accesos directos para los componentes mas utilizados
    
    -se creo un script dedicado a instanciar elementos dentro del menu scroleable

--- Assets/Script/Menus/EventsCall.cs
+++ Assets/Script/Menus/EventsCall.cs
@@ -4,20 +4,55 @@ using UnityEngine.UI;
 
 public class EventsCall : MonoBehaviour
 {
+    [SerializeField]
+    public Image image;
+
+    public Button button
+    {
+        get
+        {
+            if(_button==null)
+            {
+                _button = GetComponentInChildren<Button>();
+            }
+
+            return _button;
+        }
+    }
+
+    public event UnityEngine.Events.UnityAction listeners
+    {
+        add
+        {
+            button.onClick.AddListener(value);
+        }
+
+        remove
+        {
+            button.onClick.RemoveListener(value);
+        }
+    }
+
+    [SerializeField]
+    Button _button;
+
     public void Event(GameObject g)
     {
         print("\tAccediendo: " + g.name);
 
-        if (g.TryGetComponent(out Button b))
-        {
-            b.onClick.SetPersistentListenerState(0, UnityEngine.Events.UnityEventCallState.Off);
-            b.Event();
-            //menu.eventListVoid[b.name](b.gameObject);
-            b.onClick.Invoke();
+        if(_button==null)
+            if (!g.TryGetComponent(out _button))
+            {
+                DebugPrint.Warning("No contiene uno de los componentes esperados: " + g.name);
+                return; 
+            }
 
-            //menu.eventListVoid.Remove(b.name);
-            return;
-        }
-        DebugPrint.Warning("No contiene uno de los componentes esperados: " + g.name);
+        _button.onClick.SetPersistentListenerState(0, UnityEngine.Events.UnityEventCallState.Off);
+
+        _button.Event();
+        //menu.eventListVoid[b.name](b.gameObject);
+        _button.onClick.Invoke();
+
+        //menu.eventListVoid.Remove(b.name);
     }
 }
