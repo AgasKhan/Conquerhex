ec8f2816080910d059b7abe3537f6d9be1ef92df
Karevelli <revellikarina@hotmail.com>
2023-05-21 23:49:17 -0300

    lvl1

---
+++ Assets/Script/SpawnEnemy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemy : MonoBehaviour
+{
+    public MyEnemy enemyPref;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Alpha1))
+        {
+            enemyPref.Clone();
+        }
+
+
+    }
+}
