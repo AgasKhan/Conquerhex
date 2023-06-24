a806b614b8103b6bb2f5b3bc93f920ec4d83a3a2
lider23598 <lider.huamantuco@davinci.edu.ar>
2023-06-24 12:37:29 -0300

    Shaders

---
+++ Assets/Sprites/Scenary/Lava/Shaders/Dissolve.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dissolve : MonoBehaviour
+{
+    [SerializeField] private Material material;
+
+    private float dissolveAmount;
+    private bool isDissolve;
+
+    private void Update()
+    {
+        if (isDissolve)
+        {
+            dissolveAmount = Mathf.Clamp01(dissolveAmount + Time.deltaTime);
+            material.SetFloat("_Dessolve", dissolveAmount);
+        }
+        else
+        {
+            dissolveAmount = Mathf.Clamp01(dissolveAmount - Time.deltaTime);
+            material.SetFloat("_Dessolve", dissolveAmount);
+        }
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            isDissolve = true;
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            isDissolve = false;
+        }
+    }
+
+}
