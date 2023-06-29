5f27250d4b18d212bfd4919bacdec477becd599a
JhamilCastillo <jhamil.castillo@davinci.edu.ar>
2023-06-28 20:28:44 -0400

    Update

--- Assets/Script/Currency/StaminaSystem.cs
+++ Assets/Script/Currency/StaminaSystem.cs
@@ -5,6 +5,147 @@ using System;
 
 public class StaminaSystem : MonoBehaviour
 {
+    [SerializeField] Character character;
+
+    [SerializeField] int maxStamina = 10;
+    int currentStamina;
+    DateTime nextStaminaTime;
+    DateTime lastStaminaTime;
+    [SerializeField] float timeToCharge = 10f;
+
+    bool recharging;
+
+    int notifID;
+
+    private void Start()
+    {
+        if (PlayerPrefs.HasKey("currentStamina"))
+        {
+            Load();
+        }
+        else
+        {
+            currentStamina = maxStamina;
+            Save();
+        }
+
+        UpdateUI();
+        StartCoroutine(UpdateStamina());
+
+        #if UNITY_ANDROID
+        if (currentStamina < maxStamina)
+        {
+            notifID = NotificationsSystem.SendNotification(AddDuration(lastStaminaTime, timeToCharge * (maxStamina - currentStamina)),
+                                     "Volvé a jugar", "Se te recargó toda la energía", "stamina_ch");
+        }
+        #endif
+    }
+
+
+    IEnumerator UpdateStamina()
+    {
+        UpdateTimer();
+        recharging = true;
+        while (currentStamina < maxStamina)
+        {
+            DateTime currentTime = DateTime.Now;
+            DateTime nextTime = nextStaminaTime;
+
+
+            bool addingStamina = false;
+            while (currentTime > nextTime)
+            {
+                if (currentStamina >= maxStamina) break;
+
+
+                currentStamina += 1;
+                addingStamina = true;
+                DateTime timeToAdd = nextTime;
+
+                if (lastStaminaTime > nextTime)
+                    timeToAdd = lastStaminaTime;
+
+                nextTime = AddDuration(timeToAdd, timeToCharge);
+            }
+
+            if (addingStamina)
+            {
+                nextStaminaTime = nextTime;
+                lastStaminaTime = DateTime.Now;
+            }
+
+            UpdateUI();
+            UpdateTimer();
+            Save();
+
+            yield return new WaitForEndOfFrame();
+        }
+        recharging = false;
+    }
+
+    void UpdateTimer()
+    {
+        if (currentStamina >= maxStamina)
+        {
+            return;
+        }
+    }
+
+    public void RechargeStamina(int staminaToAdd)
+    {
+        currentStamina += staminaToAdd;
+
+        UpdateUI();
+        Save();
+        if (currentStamina >= maxStamina)
+        {
+            if (recharging)
+            {
+                recharging = false;
+                StopAllCoroutines();
+            }
+        }
+    }
+
+
+    DateTime AddDuration(DateTime date, float duration)
+    {
+        return date.AddSeconds(duration);
+    }
+
+    bool HasEnoughStamina(int stamina) => currentStamina >= stamina;
+
+    void UpdateUI()
+    {
+        character.AddOrSubstractItems("PortalFuel", currentStamina);
+    }
+
+    void Save()
+    {
+        PlayerPrefs.SetInt("currentStamina", currentStamina);
+        PlayerPrefs.SetString("nextStaminaTime", nextStaminaTime.ToString());
+        PlayerPrefs.SetString("lastStaminaTime", lastStaminaTime.ToString());
+    }
+
+    void Load()
+    {
+        currentStamina = PlayerPrefs.GetInt("currentStamina");
+        nextStaminaTime = StringToDateTime(PlayerPrefs.GetString("nextStaminaTime"));
+        lastStaminaTime = StringToDateTime(PlayerPrefs.GetString("lastStaminaTime"));
+    }
+
+    DateTime StringToDateTime(string date)
+    {
+        if (string.IsNullOrEmpty(date))
+        {
+            return DateTime.Now;
+        }
+
+        return DateTime.Parse(date);
+    }
+
+
+    /*
     [SerializeField]
     int maxStamina;
 
@@ -66,5 +207,5 @@ public class StaminaSystem : MonoBehaviour
         else
             Debug.Log("No se le dio stamina al jugador");
 
-    }
+    }*/
 }
