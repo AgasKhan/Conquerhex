117d3db2474e1971064275791d27f9d4b7266d69
JhamilCastillo <jhamil.castillo@davinci.edu.ar>
2023-07-28 02:57:14 -0400

    Turrets Update
    - Se crearon los scriptable objects y asignaron al flyweight  las armas especiales para las torretas
    - Se hizo que las torretas especifiquen en su información las habilidades que tiene el flyweight y los additive damages equipados en el caso de que los tengan
    - Se cambio la descripción del turret building para que especifique que hacen las torretas y den mas informacion al jugador

--- Assets/Script/Buildings/LogicActives/TurretInfo.cs
+++ Assets/Script/Buildings/LogicActives/TurretInfo.cs
@@ -8,12 +8,34 @@ public class TurretInfo : LogicActive<Building>
     {
         var aux = specificParam[0];
 
-        aux.myBuildSubMenu.detailsWindow.SetTexts("", aux.flyweight.GetDetails()["Description"]);
-
         if (aux.currentLevel == 0)
+        {
+            aux.myBuildSubMenu.detailsWindow.SetTexts("", aux.flyweight.GetDetails()["Description"]);
             aux.myBuildSubMenu.detailsWindow.SetImage(((TurretBuild)aux).baseSprite);
+        } 
         else
+        {
+            string newText = "\nHabilidades: \n".RichText("color", "#00ffffff"); ;
+
+            foreach (var item in aux.flyweight.kataCombos)
+            {
+                //Filtrar cual tiene equipada el jugador 
+
+                newText += item.kata.nameDisplay.RichText("color", "#ffa500ff") +"\n";
+                newText += item.kata.GetDetails().ToString();
+            }
+
+            if(aux.flyweight.additiveDamage.Length > 0)
+            {
+                newText += "\nAdditive Damage: ".RichText("color", "#00ffffff");
+                //newText += aux.flyweight.additiveDamage[0].typeInstance.ToString() + " x " + aux.flyweight.additiveDamage[0].ToString();
+                newText += aux.flyweight.GetDetails()["Description"].ToString() + " x " + aux.flyweight.additiveDamage[0].ToString();
+            } 
+
+            aux.myBuildSubMenu.detailsWindow.SetTexts("Turret " + ((TurretBuild)aux).originalAbility, newText);
             aux.myBuildSubMenu.detailsWindow.SetImage(((TurretBuild)aux).possibleAbilities[((TurretBuild)aux).originalAbility][aux.currentLevel - 1]);
+
+        }
 
         aux.myBuildSubMenu.DestroyCraftButtons();
     }
