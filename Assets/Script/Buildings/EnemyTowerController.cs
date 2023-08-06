0f91dca2b5789d38519376fa1202d7366b774be3
JhamilCastillo <jhamil.castillo@davinci.edu.ar>
2023-08-06 00:12:57 -0400

    Diseño de Niveles
    - Se creo el prefab para las torretas enemigas
    - Se hicieron las modificaciones necesarias en el nivel 2
    - Se creo el prefab variant del boss minotauro
    - Se creo el flyweight del boss

---
+++ Assets/Script/Buildings/EnemyTowerController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTowerController : TurretController
+{
+    protected override void Config()
+    {
+        base.Config();
+        MyStarts = MyStart;
+    }
+
+    void MyStart()
+    {
+        for (int i = 0; i < turret.myStructure.kataCombos.Length; i++)
+        {
+            turret.SetKataCombo(i);
+        }
+
+        turret.interactuable = false;
+    }
+
+    protected override void DestroyTurret()
+    {
+        base.DestroyTurret();
+        transform.GetChild(0).SetActiveGameObject(false);
+        turret.visible = false;
+    }
+
+}
