99d58589366bc1b97ee8333485d79b3d3a66c81c
JhamilCastillo <jhamil.castillo@davinci.edu.ar>
2023-09-12 19:00:11 -0400

    Fix Ads
    - Se hicieron los cambio s necesarios para que los ads no tiren error en esta versión

--- Assets/Script/Buildings/AdsBuildController.cs
+++ Assets/Script/Buildings/AdsBuildController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Advertisements;
 
-public class AdsBuildController : BuildingsController//, IUnityAdsListener
+public class AdsBuildController : BuildingsController, IUnityAdsShowListener
 {
     [SerializeField] string adToShow = "Rewarded_Android";
 
@@ -15,29 +15,31 @@ public class AdsBuildController : BuildingsController//, IUnityAdsListener
     }
     void MyAwake()
     {
-        //Advertisement.AddListener(this);
+        Advertisement.Initialize(adToShow);
+
         if (Application.platform == RuntimePlatform.Android)
             Advertisement.Initialize("5307781");
         else
             Advertisement.Initialize("5307781", true);
     }
-    /*
+    
     public void ShowAd()
     {
-        if (!Advertisement.IsReady())
+        if (!Advertisement.isSupported)
         {
             Debug.Log("No hay Ad");
             return;
         }
 
         Advertisement.Show(adToShow);
-    }*/
+    }
 
     public override void EnterBuild()
     {
-       // ShowAd();
+       ShowAd();
     }
 
+    /*
     public void OnUnityAdsDidError(string message)
     {
 
@@ -61,6 +63,10 @@ public class AdsBuildController : BuildingsController//, IUnityAdsListener
         }
 
     }
+    */
+
+    //------------------
+
 
     public void OnUnityAdsDidStart(string placementId)
     {
@@ -72,4 +78,36 @@ public class AdsBuildController : BuildingsController//, IUnityAdsListener
 
     }
 
+    public void OnUnityAdsShowFailure(string placementId, UnityAdsShowError error, string message)
+    {
+        Debug.Log("Hubo un error al cargar el Ad");
+    }
+
+    public void OnUnityAdsShowStart(string placementId)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnUnityAdsShowClick(string placementId)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnUnityAdsShowComplete(string placementId, UnityAdsShowCompletionState showCompletionState)
+    {
+        if (placementId != "Rewarded_Android")
+            return;
+
+        if (showCompletionState == UnityAdsShowCompletionState.COMPLETED)
+        {
+            Debug.Log("Te doy una recompensa");
+            GameManager.instance.playerCharacter.AddOrSubstractItems("PortalFuel", 10);
+        }
+
+        else
+        {
+            Debug.Log("No te doy nada");
+            MenuManager.instance.modulesMenu.ObtainMenu<PopUp>(false).SetActiveGameObject(true).SetWindow("", "No conseguiste la recompensa por saltar el anuncio").AddButton("Cerrar", () => MenuManager.instance.modulesMenu.ObtainMenu<PopUp>(false));
+        }
+    }
 }
