3414705602531401a18ba59bec48e7cbfc93723f
JhamilCastillo <jhamil.castillo@davinci.edu.ar>
2024-03-23 04:12:19 -0400

    Custom Columns
    - Se creó la clase Custom Columns con la que se pueden personalizar columnas de texto e imprimirlas en pantalla con espaciados coherentes
    - Todos los operadores de suma en diferente orden sirven con las Custom Colums y cualquier IEnumerable de strings

---
+++ Assets/Script/Entity/InteractEntityComponent.cs
@@ -0,0 +1,25 @@
+using ComponentsAndContainers;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractEntityComponent : ComponentOfContainer<Entity>
+{
+
+
+
+    public override void OnEnterState(Entity param)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void OnExitState(Entity param)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void OnStayState(Entity param)
+    {
+        throw new System.NotImplementedException();
+    }
+}
--- Assets/Script/Menus/InventorySubMenu.cs
+++ Assets/Script/Menus/InventorySubMenu.cs
@@ -84,12 +84,22 @@ public class InventorySubMenu : CreateSubMenu
 
                        var titulos = new string[] { "Character damages", "operacion", "Weapon equiped damages", "operacion", "Kata Selected" };
 
+                       var test1 = new CustomColumns(titulos);
+                       Debug.Log(test1.ToString());
+
                        mainText += FormatColumns(titulos);
 
                        mainText += FormatColumns(MaxLengths(titulos), characterDmgs, "+", weaponDmgs, "x", kataDmgs);
 
                        var totalDamage = Damage.Combine(Damage.AdditiveFusion, auxKata.weaponEnabled.itemBase.damages, character.caster.additiveDamage.content);
                        var resultDmgs = totalDamage.ToArray().ToString(": ", "\n");
+
+                       var test2 = new CustomColumns(totalDamage.ToArray().ToString(": ", "\n"));
+                       Debug.Log(test2.ToString());
+
+                       //var test3 = test1 + test2;
+                       Debug.Log((totalDamage.ToArray().ToString(": ", "\n")).ToString() + test1);
+                       Debug.Log((test2 + test1).ToString());
                        //mainText += resultDmgs;
 
                        mainText += "\nCharacter and weapon combined damages:\n";
@@ -256,6 +266,190 @@ public class InventorySubMenu : CreateSubMenu
         }
 
         return details;
+    }
+
+}
+
+public class CustomColumns
+{
+    int maxRows;
+    List<int> maxLengths;
+    List<List<string>> columns = new List<List<string>>();
+
+    public override string ToString()
+    {
+        string result = "";
+
+        for (int i = 0; i < maxRows; i++)
+        {
+            for (int j = 0; j < columns.Count; j++)
+            {
+                string linea = i < columns[j].Count ? columns[j][i] : "";
+                result += FormatString(linea, maxLengths[j]) + "\t";
+            }
+            result += "\n";
+        }
+
+        return result;
+    }
+
+    public static CustomColumns operator + (CustomColumns este, CustomColumns otro)
+    {
+        int index = 0;
+
+        foreach (var item in otro.columns)
+        {
+            if (index >= este.columns.Count)
+            {
+                este.columns.Add(new List<string>());
+                este.maxLengths.Add(0);
+            }
+
+            este.columns[index].AddRange(otro.columns[index]);
+            este.maxLengths[index] = Mathf.Max(este.maxLengths[index], otro.maxLengths[index]);
+            index++;
+        }
+        /*
+        for (int i = 0; i < Mathf.Max(este.columns.Count, otro.columns.Count) ; i++)
+        {
+            Debug.Log("Este: " + (este.columns[i]==null) + " Otro: " + (otro.columns[i] == null) + " Index: " + i);
+            Debug.Log(Mathf.Max(este.columns.Count, otro.columns.Count));
+
+            if (i >= este.columns.Count)
+            {
+                este.columns.Add(new List<string>());
+                este.maxLengths.Add(0);
+            }
+
+
+            este.columns[i].AddRange(otro.columns[i]);
+            este.maxLengths[i] = Mathf.Max(este.maxLengths[i], otro.columns[i].Count);
+        }
+        */
+        este.maxRows += otro.maxRows;
+        return este;
+    }
+    public static CustomColumns operator + (CustomColumns este, IEnumerable<string> otro)
+    {
+        return AddRow(otro, este, (lista, item) => { lista.Add(item); });
+    }
+
+    public static CustomColumns operator + (IEnumerable<string> otro, CustomColumns este)
+    {
+        return AddRow(otro, este, (lista, item) => { lista.Insert(0, item); });
+    }
+
+    public static CustomColumns AddRow(IEnumerable<string> otro, CustomColumns este, System.Action<List<string>, string> action)
+    {
+        int index = 0;
+        foreach (var item in otro)
+        {
+            if (index >= este.columns.Count)
+            {
+                este.columns.Add(new List<string>());
+                este.maxLengths.Add(0);
+            }
+
+            action?.Invoke(este.columns[index], item);
+            este.maxLengths[index] = Mathf.Max(este.maxLengths[index], item.Length);
+
+            index++;
+        }
+        este.maxRows++;
+
+        return este;
+    }
+
+    public void AddFirst(string texto, int columnIndex)
+    {
+        if (columnIndex >= 0 && columnIndex < columns.Count)
+        {
+            columns[columnIndex].Insert(0, texto);
+            EqualizeRows();
+        }
+        else
+            Debug.LogError("Indice de columna fuera de rango");
+    }
+
+
+    public void AddLast(string texto, int columnIndex)
+    {
+        if (columnIndex >= 0 && columnIndex < columns.Count)
+        {
+            for (int i = 0; i < columns[columnIndex].Count; i++)
+            {
+                if (columns[columnIndex][i].Replace(" ", "") == "")
+                {
+                    columns[columnIndex][i] = FormatString(texto, maxLengths[columnIndex]);
+                    return;
+                }
+            }
+
+            columns[columnIndex].Add(texto);
+
+            if (columns[columnIndex].Count > maxRows)
+                EqualizeRows();
+        }
+        else
+        {
+            Debug.LogError("Indice de columna fuera de rango");
+        }
+    }
+
+    private void EqualizeRows()
+    {
+        for (int i = 0; i < columns.Count; i++)
+        {
+            if (columns[i].Count < maxRows)
+            {
+                columns[i].Add(FormatString("", maxLengths[i]));
+            }
+        }
+    }
+
+    List<int> MaxLengths()
+    {
+        maxRows = 0;
+        maxLengths = new List<int>();
+
+        for (int i = 0; i < columns.Count; i++)
+        {
+            maxRows = Mathf.Max(maxRows, columns[i].Count);
+            Debug.Log("Max Lengths: " + maxLengths.Count + " Columns: " + columns.Count + " Index: "+i);
+            maxLengths.Add(GetMaxLength(columns[i]));
+        }
+
+        return maxLengths;
+    }
+
+    int GetMaxLength(IEnumerable<string> lineas)
+    {
+        int maxLength = 0;
+        foreach (string linea in lineas)
+        {
+            maxLength = Mathf.Max(maxLength, linea.FixedLength());
+        }
+        return maxLength;
+    }
+
+    string FormatString(string texto, int longitudMaxima)
+    {
+        int largo = texto.FixedLength();
+
+        if (largo <= longitudMaxima)
+            return texto + new string(' ', (longitudMaxima - largo));
+        return texto.Substring(0, longitudMaxima - 3) + "...";
+    }
+
+    public CustomColumns(params string[] columnas)
+    {
+        for (int j = 0; j < columnas.Length; j++)
+        {
+            columns.Add(new List<string>());
+            columns[j].AddRange(columnas[j].Split('\n'));
+        }
+
+        MaxLengths();
     }
 
 }
