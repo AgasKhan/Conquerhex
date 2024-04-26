a357b42b083529bc8236043d6ff7f3ad9a855fe3
AgasKhan <lucas_galardo_8@yahoo.com.ar>
2024-04-25 21:02:36 -0300

    UI health Update
    -Se hizo mas responsiva la barra, en cuanto a la regeneracion y al danio

--- Assets/Script/View/Interfaz.cs
+++ Assets/Script/View/Interfaz.cs
@@ -88,19 +88,44 @@ namespace UI
 
         private void healthBarUpdate(Health health)
         {
-            vida.FillAmount = health.actualLife / health.maxLife;
+            float nextRegenLifePercentage = health.nextRegenLife / health.maxLife;
+
+            float healthPercentage = health.actualLife / health.maxLife;
+
+            float cooldownRegen = (1 - health.actualCoolDownRegen / health.MaxCoolDownRegen);
 
             textVida.text = ((int)health.actualLife).ToString();
 
-            regen.FillAmount = health.actualRegen / 100;
+            regen.FillAmount = health.actualRegen / 100f;
 
-            regenMax.FillAmount = health.maxRegen / 100;
+            regenMax.FillAmount = health.maxRegen / 100f;
 
             textRegen.text = ((int)(health.actualRegen / 100 * health.maxLife)).ToString();
 
-            regenTimeMax.FillAmount = health.nextRegenLife / health.maxLife;
+            regenTime.FillAmount = cooldownRegen * nextRegenLifePercentage;
 
-            regenTime.FillAmount = (1 - health.actualCoolDownRegen/health.MaxCoolDownRegen) * regenTimeMax.FillAmount;
+            if (cooldownRegen == 0 || cooldownRegen > 0.05f)
+            {
+                var aux = (Mathf.Clamp((cooldownRegen - (0.05f)) * 10, 0, 1));
+
+                regenTimeMax.FillAmount = Mathf.Clamp((health.nextRegenLife * aux) / health.maxLife, healthPercentage, 1);
+            }
+
+            if(vida.FillAmount > healthPercentage)
+            {
+                if (cooldownRegen < 0.05f)
+                {
+                    vida.FillAmount = Mathf.Lerp(vida.FillAmount, healthPercentage, Time.deltaTime);
+                }
+                else
+                {
+                    vida.FillAmount = Mathf.Lerp(vida.FillAmount, healthPercentage, Time.deltaTime*10);
+                }
+            }
+            else
+            {
+                vida.FillAmount = healthPercentage;
+            }
         }
 
         IEnumerator MyCoroutine(System.Action<bool> end, System.Action<string> msg)
