589c570de4edefa13499eb235dca7739db5f961c
JhamilCastillo <jhamil.castillo@davinci.edu.ar>
2024-04-28 23:05:59 -0400

    Update Entity.cs

--- Assets/Script/Entity/Entity.cs
+++ Assets/Script/Entity/Entity.cs
@@ -238,7 +238,7 @@ public abstract class Entity : Container<Entity>, IDamageable, IGetEntity, ISave
 
     public void Teleport(Hexagone hexagone, int lado)
     {
-        Debug.Log("Al hacer tp de: " + name + " el hexagone parent es: " + (hexagoneParent!=null));
+        //Debug.Log("Al hacer tp de: " + name + " el hexagone parent es: " + (hexagoneParent!=null));
         if (hexagone == hexagoneParent)
             return;
 
