c9b2a3eddc7e268a040965609de85634b2722b25
JhamilCastillo <jhamil.castillo@davinci.edu.ar>
2024-06-11 15:43:07 -0400

    Update
    - Ahora el dirigible es un arbol sangrante y muestra el final
    - Se seteo el mapa para que se generen 12 hexagonos

--- Assets/Script/Old/Salida.cs
+++ Assets/Script/Old/Salida.cs
@@ -5,20 +5,36 @@ using UnityEngine;
 public class Salida : MonoBehaviour
 {
     //public Menu menu;
+    /*
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.CompareTag("Player"))
         {
             GameManager.instance.Menu(true);
+    */
+            //var aux = collision.GetComponent<Character>();
+            //MenuManager.instance.modulesMenu.ObtainMenu<PopUp>(true).SetWindow("Victoria", "Has logrado llegar hasta el dirigible para escapar \n\n"+ "¿Deseas volver a la base con todo lo recolectado?".RichText("color", "#00ffffff"))
 
-            var aux = collision.GetComponent<Character>();
-            MenuManager.instance.modulesMenu.ObtainMenu<PopUp>(true).SetWindow("Victoria", "Has logrado llegar hasta el dirigible para escapar \n\n"+ "¿Deseas volver a la base con todo lo recolectado?".RichText("color", "#00ffffff"))
+            
             /*.AddButton("Si", () => { aux.inventory.AddOrSubstractItems("Coin", 10); LoadSystem.instance.LoadAndSavePlayer("Base", true); })
-            .AddButton("No", () => { MenuManager.instance.modulesMenu.ObtainMenu<PopUp>(false); GameManager.instance.Menu(false);})*/;
+            .AddButton("No", () => { MenuManager.instance.modulesMenu.ObtainMenu<PopUp>(false); GameManager.instance.Menu(false);})*/
+            /*;
 
             //menu.ShowMenu("Victoria!", "Has logrado llegar hasta el dirigible para escapar", false, true);
             //Time.timeScale = 0;
         }
+    }*/
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GameManager.instance.Menu(true);
+        if (other.CompareTag("Player"))
+        {
+            MenuManager.instance.modulesMenu.ObtainMenu<PopUp>(false).SetActiveGameObject(true)
+               .SetWindow("Demo terminada", "Gracias por pasarte la demo de Arrange the Heaven \n\n¿Deseas cerrar el juego?")
+               .AddButton("Si", Application.Quit)
+               .AddButton("No", () => { GameManager.instance.Menu(false); MenuManager.instance.modulesMenu.ObtainMenu<PopUp>(false).SetActiveGameObject(false); });
+        }
     }
-    
+
 }
