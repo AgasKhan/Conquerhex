c792c207293540ab4d94a988c947f90c4979b5a5
JhamilCastillo <jhamil.castillo@davinci.edu.ar>
2024-06-24 23:36:35 -0400

    Árbol dorado Update
    - Ahora el árbol dorado debe ser destruido para ser interactuable y permitirse salir de la aplicación o seguir jugando
    - Ahora el script Salida hereda de Building y el árbol dorado es una entidad que al morir se vuelve interactuable

--- Assets/Script/Old/Salida.cs
+++ Assets/Script/Old/Salida.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Salida : MonoBehaviour
+public class Salida : Building
 {
     //public Menu menu;
     /*
@@ -12,29 +12,50 @@ public class Salida : MonoBehaviour
         {
             GameManager.instance.Menu(true);
     */
-            //var aux = collision.GetComponent<Character>();
-            //MenuManager.instance.modulesMenu.ObtainMenu<PopUp>(true).SetWindow("Victoria", "Has logrado llegar hasta el dirigible para escapar \n\n"+ "¿Deseas volver a la base con todo lo recolectado?".RichText("color", "#00ffffff"))
+    //var aux = collision.GetComponent<Character>();
+    //MenuManager.instance.modulesMenu.ObtainMenu<PopUp>(true).SetWindow("Victoria", "Has logrado llegar hasta el dirigible para escapar \n\n"+ "¿Deseas volver a la base con todo lo recolectado?".RichText("color", "#00ffffff"))
 
-            
-            /*.AddButton("Si", () => { aux.inventory.AddOrSubstractItems("Coin", 10); LoadSystem.instance.LoadAndSavePlayer("Base", true); })
-            .AddButton("No", () => { MenuManager.instance.modulesMenu.ObtainMenu<PopUp>(false); GameManager.instance.Menu(false);})*/
-            /*;
 
-            //menu.ShowMenu("Victoria!", "Has logrado llegar hasta el dirigible para escapar", false, true);
-            //Time.timeScale = 0;
-        }
-    }*/
+    /*.AddButton("Si", () => { aux.inventory.AddOrSubstractItems("Coin", 10); LoadSystem.instance.LoadAndSavePlayer("Base", true); })
+    .AddButton("No", () => { MenuManager.instance.modulesMenu.ObtainMenu<PopUp>(false); GameManager.instance.Menu(false);})*/
+    /*;
 
-    private void OnTriggerEnter(Collider other)
+    //menu.ShowMenu("Victoria!", "Has logrado llegar hasta el dirigible para escapar", false, true);
+    //Time.timeScale = 0;
+}
+}*/
+    [SerializeField]
+    SpriteRenderer sprite;
+
+    protected override void Config()
+    {
+        base.Config();
+
+        MyAwakes += MyAwake;
+    }
+
+    void MyAwake()
+    {
+        //interactComp.OnInteract += SetRewards;
+        health.noLife += Health_noLife;
+        health.death += Health_noLife;
+    }
+
+    private void Health_noLife()
+    {
+        interactComp.ChangeInteract(true);
+        team = Team.noTeam;
+
+        sprite.color = Color.yellow;
+    }
+
+    public void Victory()
     {
         GameManager.instance.Menu(true);
-        if (other.CompareTag("Player"))
-        {
-            MenuManager.instance.modulesMenu.ObtainMenu<PopUp>(false).SetActiveGameObject(true)
+        MenuManager.instance.modulesMenu.ObtainMenu<PopUp>(false).SetActiveGameObject(true)
                .SetWindow("Demo terminada", "Gracias por pasarte la demo de Arrange the Heaven \n\n¿Deseas cerrar el juego?")
                .AddButton("Si", Application.Quit)
                .AddButton("No", () => { GameManager.instance.Menu(false); MenuManager.instance.modulesMenu.ObtainMenu<PopUp>(false).SetActiveGameObject(false); });
-        }
-    }
 
+    }
 }
