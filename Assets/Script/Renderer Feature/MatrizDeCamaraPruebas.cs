d599471b0ab8e2f0fb6ef3c92716295d83271a41
AgasKhan <lucas_galardo_8@yahoo.com.ar>
2024-09-08 00:49:05 -0300

    Prueba aun no aceptable

--- Assets/Script/Renderer Feature/MatrizDeCamaraPruebas.cs
+++ Assets/Script/Renderer Feature/MatrizDeCamaraPruebas.cs
@@ -1,4 +1,3 @@
-
 using System;
 using UnityEngine;
 using UnityEngine.Rendering;
@@ -8,14 +7,11 @@ public class MatrizRenderFeature : ScriptableRendererFeature
 {
     public class MatrizDeCamaraPruebas : ScriptableRenderPass
     {
-        RenderTargetIdentifier[] _renderTextures;
+        Data data;
 
-        Matrix4x4 _view;
-        Matrix4x4 _proj;
-
-        public MatrizDeCamaraPruebas(RenderTargetIdentifier[] render)
+        public MatrizDeCamaraPruebas(Data data)
         {
-            _renderTextures = render;
+            this.data = data;
         }
 
         public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
@@ -26,60 +22,106 @@ public class MatrizRenderFeature : ScriptableRendererFeature
             // _view = Matrix4x4.LookAt(cam.transform.position, Character, el up de la camara);
             //_proj = Matrix4x4.Perspective(cam.fieldOfView, cam.aspect, cam.nearClipPlane, cam.farClipPlane);
 
-            _view = renderingData.cameraData.camera.worldToCameraMatrix;
-            _proj = renderingData.cameraData.camera.projectionMatrix;
+            data._view = renderingData.cameraData.camera.worldToCameraMatrix;
+            data._proj = renderingData.cameraData.camera.projectionMatrix;
 
+            Debug.Log(data._view);
+            Debug.Log(renderingData.cameraData.camera.cullingMatrix);
+            Debug.Log("///////////////////////////");
 
-            for (int i = 0; i < _renderTextures.Length; i++)
+            //for (int i = 0; i < _renderTextures.Length; i++)
             {
-                cmd.SetRenderTarget(_renderTextures[i]);
+                cmd.SetRenderTarget(data._renderTextures[0]);
+                cmd.ClearRenderTarget(true, true, Color.clear);
+                data._view = GetModifiedViewMatrix(0, renderingData.cameraData.camera.transform.position);
 
-                Matrix4x4 viewMatrix = GetModifiedViewMatrix(i);
 
-                cmd.SetViewProjectionMatrices(viewMatrix, _proj);
-                cmd.ClearRenderTarget(true, true, Color.black);
+                Debug.Log(data._view);
+
+                cmd.SetViewProjectionMatrices(data._view, data._proj);
+
+                renderingData.cameraData.camera.TryGetCullingParameters(out var scriptableCullingParameters);
+
+                scriptableCullingParameters.origin = data._position;
+
+                var cullResults = context.Cull(ref scriptableCullingParameters);
+
+                context.ExecuteCommandBuffer(cmd);
+                cmd.Clear();
+                //Debug.Log(data._view);
+
+               
+
+                DrawingSettings drawingSettings = CreateDrawingSettings(new ShaderTagId("UniversalForward"), ref renderingData, SortingCriteria.CommonOpaque);
+                FilteringSettings filteringSettings = new(RenderQueueRange.all, renderingData.cameraData.camera.cullingMask);
+
+                context.DrawRenderers(cullResults, ref drawingSettings, ref filteringSettings);
 
 
                 context.ExecuteCommandBuffer(cmd);
                 cmd.Clear();
             }
 
+            
 
 
             CommandBufferPool.Release(cmd);
         }
 
-        Matrix4x4 GetModifiedViewMatrix(int index)
+        //projection: perspective or orthogonal
+        //
+
+        Matrix4x4 GetModifiedViewMatrix(int index, Vector3 pos)
         {
             //Ver de cambiar la posicion de la camara o cambiar el codigo... Temporal
-            Vector3 positionOffset = new Vector3(index * 2, index * 2, 0);
+            //Vector3 positionOffset = new Vector3(index * 2, index * 2, 0);
+
+            //Matrix4x4.TRS(_position, _rotation, Vector3.one);
 
             //Hay que cambiar la posicion de translate de camara con la posicion deseada
-            return Matrix4x4.Translate(positionOffset) * _view;
+
+            Vector3 deltaPosition = (pos - data._position);
+
+            deltaPosition.x *= -1;
+            deltaPosition.y *= -1;
+
+            Matrix4x4 m = data._view * Matrix4x4.TRS(deltaPosition, data._rotation, data._scale);
+
+            return m;
         }
     }
 
     MatrizDeCamaraPruebas _pass;
-    public RenderTexture[] ren;
+    public Data data;
 
+    [System.Serializable]
+    public class Data
+    {
+        public RenderTexture[] _renderTextures;
+
+        
+        public Quaternion _rotation = Quaternion.identity;
+
+        public Vector3 _scale;
+        
+        public Vector3 _position;
+
+        public Matrix4x4 _view;
+        public Matrix4x4 _proj;
+    }
 
 
     public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
     {
+
+        if (!renderingData.cameraData.camera.CompareTag("MainCamera"))
+            return;
+
         renderer.EnqueuePass(_pass);
-
     }
-
     public override void Create()
     {
-
-        RenderTargetIdentifier[] ids = new RenderTargetIdentifier[ren.Length];
-
-        for (int i = 0; i < ren.Length; i++)
-        {
-            ids[i] = new RenderTargetIdentifier(ren[i]);
-        }
-
-        _pass = new MatrizDeCamaraPruebas(ids);
+        _pass = new MatrizDeCamaraPruebas(data);
     }
+
 }
