acff1b36427e78396b43d376324c3bf781cba384
Rar <ramiro.mouro@davinci.edu.ar>
2024-10-17 01:54:02 -0300

    Scripted Intro Update
    -Se creo un flyweight para el personaje con todo desbloqueado.
    -Cuando se rompe un item, sale una notificacion para avisar al jugador.
    ***Provisorio: Al matar 20 enemigos, el personaje se muere y se transporta hasta la escena principal. Lo planeado es cuando los enemigos puedan atacar, estos empiecen a dañar al personaje hasta matarlo inevitablemente.

--- Assets/Script/Managers/DirectorManager.cs
+++ Assets/Script/Managers/DirectorManager.cs
@@ -8,12 +8,20 @@ public class DirectorManager : SingletonMono<DirectorManager>
     [SerializeField] ReSpawner[] _spawners;
 
     int enemiesKilled;
+    private int blocked;
 
-    Timer t;
+    private Hexagone _initial;
+
+    private Timer t;
 
     public void MyAwake()
     {
-       Spawning();
+        Spawning();
+        _player.health.death += () =>
+        {
+            t = TimersManager.Create(1f,
+                () => UnityEngine.SceneManagement.SceneManager.LoadScene("Hexagonos test"));
+        };
     }
 
     void Spawning()
@@ -23,36 +31,51 @@ public class DirectorManager : SingletonMono<DirectorManager>
         {
             Entity e = item.TryRespawn();
             e.health.death += BreakItems;
+            e.GetComponent<IA_GenericEnemy>().attackDetection.maxRadius = 100;
+            
         }
     }
 
     [ContextMenu("BreakItems")]
     void BreakItems()
     {
+        BreakRandomCombo();
+        
+        
         enemiesKilled++;
+        Debug.Log(enemiesKilled);
         if(enemiesKilled % 2 != 0) return;
 
         BreakRandomAbility();
-        BreakRandomCombo();
         BreakRandomKataCombo();
+        
+        if(enemiesKilled >= 20) _player.TakeDamage(Damage.Create<DamageTypes.Perforation>(100));
     }
 
     public void BreakRandomAbility()
     {
         int ran = Random.Range(2, _player.caster.abilities.Count);
 
+        if (_player.caster.abilities[ran].isBlocked == true) return;
+        
+        UI.Interfaz.instance["Notificacion"]
+            .ShowMsg(
+                $"La habilidad {_player.caster.abilities[ran].defaultItem?.nameDisplay} se ha roto!"
+                    .RichTextColor(Color.red));
         _player.caster.abilities[ran].isBlocked = true;
         _player.caster.abilities[ran].equiped?.Unequip();
-        Debug.Log("Habilidad rota!");
     }
 
     public void BreakRandomCombo()
     {
         int ran = Random.Range(1, _player.caster.combos.Count);
+        if (ran == 5 || ran == 10) ran++; //Hardcodeado para que los combos del click izq no se bloqueen
+
+        if (_player.caster.combos[ran].isBlocked == true) return;
 
         _player.caster.combos[ran].isBlocked = true;
         _player.caster.combos[ran].equiped?.Unequip();
-        Debug.Log("Combo roto!");
+        UI.Interfaz.instance["Notificacion"].ShowMsg($"Un combo se ha perdido!".RichTextColor(Color.red));
     }
 
     [ContextMenu("BreakWeapons")]
@@ -60,20 +83,24 @@ public class DirectorManager : SingletonMono<DirectorManager>
     {
         int ran = Random.Range(1, _player.caster.weapons.Count);
 
+        //Ver como eliminar un arma del inventario
         _player.caster.actualWeapon?.Unequip();
-        _player.inventory.SubstracItems(_player.caster.actualWeapon.itemBase, 1);
-        
-        Debug.Log("Arma rota!");
+        _player.caster.weapons[ran].inventoryComponent[0].Destroy();
+
+        UI.Interfaz.instance["Notificacion"]
+            .ShowMsg($"el arma {_player.caster.actualWeapon.nameDisplay} se ha roto!".RichTextColor(Color.red));
     }
 
     public void BreakRandomKataCombo()
     {
         int ran = Random.Range(1, _player.caster.katas.Count);
 
+        if (_player.caster.katas[ran].isBlocked == true) return;
+
         _player.caster.katas[ran].isBlocked = true;
         _player.caster.katas[ran].equiped?.Unequip();
-        Debug.Log("Kata rota!");
+        UI.Interfaz.instance["Notificacion"]
+            .ShowMsg(
+                $"la kata {_player.caster.katas[ran].defaultItem?.nameDisplay} se ha roto!".RichTextColor(Color.red));
     }
-
-
 }
